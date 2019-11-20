fa2e9fd0ac6a990943b2c062c732c0543a27c5c7
BKGoh <bkgoh@mmu.edu.my>
2019-11-20 12:58:11 +0800

    Updates

---
+++ precache-manifest.6255acc5225489059fc46607f3cb358a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b43886291e415fdfc1d6c0e59c1636a",
+    "url": "/react-app-training/index.html"
+  },
+  {
+    "revision": "e4777b333ba3833744f3",
+    "url": "/react-app-training/static/css/main.22de9252.chunk.css"
+  },
+  {
+    "revision": "a0ba525f2d9f93fa2323",
+    "url": "/react-app-training/static/js/2.44fa4a56.chunk.js"
+  },
+  {
+    "revision": "e4777b333ba3833744f3",
+    "url": "/react-app-training/static/js/main.8f92d852.chunk.js"
+  },
+  {
+    "revision": "b6003d445e04bb6b0009",
+    "url": "/react-app-training/static/js/runtime-main.12948529.js"
+  }
+]);
