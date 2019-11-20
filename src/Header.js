cf077cc6a5a943a890739c53d53751a401dbe282
BKGoh <bkgoh@mmu.edu.my>
2019-11-20 12:13:14 +0800

    Updated link for navigation

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,7 @@ function Header(props) {
     return (
         <div class="navbar navbar-dark bg-dark shadow-sm">
             <div class="container d-flex justify-content-between">
-            <a href="#" class="navbar-brand d-flex align-items-center">
+            <a href="/" class="navbar-brand d-flex align-items-center">
                 <strong>{props.siteName}</strong>
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
